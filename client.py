6e2911f39eb31b9350678a3fc7443f0074d9f9c7
Holovata Karyna <k.golovata1505@gmail.com>
2024-05-31 21:59:56 +0300

    Client and Server

--- client.py
+++ client.py
@@ -1,40 +1,30 @@
+import os
+import json
 import base64
 import datetime
 import random
 import string
 import asyncio
 import websockets
-import json
+
 import diffie_hellman as dh
 
-from controller import EncryptionController
 from cryptography.hazmat.primitives.asymmetric.dh import DHParameterNumbers
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
 
+from controller import EncryptionController
 
-encryption_ctrl = None
+encryptionController = None
 user = None
 
-WAITING = "waiting"
-ACTIVE = "active"
-KEY_SETUP_PHASE1 = "setup1"
-KEY_SETUP_PHASE2 = "setup2"
-state = WAITING
+# Константи для станів
+WAITING = "WAITING"
+ACTIVE = "ACTIVE"
+KEY_SETUP_PHASE1 = "KEY_SETUP_PHASE1"
+KEY_SETUP_PHASE2 = "KEY_SETUP_PHASE2"
 
-# Дані для першого користувача
-pub_keys = {}  # Публічні ключі учасників
-shared_keys = {}  # Спільні ключі учасників
-sym_keys = {}  # Симетричні ключі учасників
-salts = {}  # Солі учасників
-
-my_sym_key = None  # Власний симетричний ключ
-my_salt = None  # Власна сіль
-
-SHARED_KEY = None  # Загальний ключ
-
-participant_count = 0  # Кількість учасників
-processed_count = 0  # Лічильник оброблених учасників
+current_state = WAITING
 
 # Параметри для алгоритму Диффі-Хеллмана
 p = 0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AACAA68FFFFFFFFFFFFFFFF
@@ -43,129 +33,154 @@ g = 2
 params_numbers = DHParameterNumbers(p, g)
 parameters = params_numbers.parameters(default_backend())
 
-# Генерація приватного та публічного ключів для користувача
-priv_key = dh.generate_private_key(parameters)
-pub_key = dh.generate_public_key(priv_key)
+# Генерація приватного та публічного ключів
+private_key = dh.generate_private_key(parameters)
+public_key = dh.generate_public_key(private_key)
+
+participants_number = 0
+processed_participants_number = 0
+
+# Дані для першого користувача
+pub_keys = {}
+shared_keys = {}
+sym_keys = {}
+salts = {}
+
+# Дані для непершого користувача
+my_sym_key = None
+my_salt = None
+
+shared_key = None
 
 
-# Функція для серіалізації публічного ключа у строку
-def serialize_pub_key(pub_key):
-    pub_bytes = pub_key.public_bytes(
+# Функція для серіалізації публічного ключа в рядок
+def serialize_key(pub_key):
+    public_bytes = pub_key.public_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PublicFormat.SubjectPublicKeyInfo,
     )
-    return base64.b64encode(pub_bytes).decode("utf-8")
+    return base64.b64encode(public_bytes).decode("utf-8")
 
 
-# Функція для десеріалізації строки у публічний ключ
-def deserialize_pub_key(pub_key_str):
-    pub_bytes = base64.b64decode(pub_key_str.encode("utf-8"))
-    return serialization.load_pem_public_key(pub_bytes)
+# Функція для десеріалізації рядка в публічний ключ
+def deserialize_key(pub_key_str):
+    public_bytes = base64.b64decode(pub_key_str.encode("utf-8"))
+    return serialization.load_pem_public_key(public_bytes)
 
 
-# Функція для генерації юзернейму
-def create_username():
+# Функція для генерації унікального імені користувача
+def generate_username():
     timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
-    return f"user_{timestamp}_{random_string}"
+    random_str = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+    return f"user_{timestamp}_{random_str}"
 
 
-'''
-async def send_user_msg(ws):
-    global encryption_ctrl, state
+# Основна функція
+async def main():
+    global pub_keys, shared_keys, sym_keys, salts
+    global participants_number, processed_participants_number, my_sym_key, my_salt, shared_key
+    global public_key, private_key, encryptionController, user
 
-    while state == ACTIVE:
-        message = await asyncio.to_thread(input, "Введіть повідомлення: ")
-        message_json = encryption_ctrl.create_message_json(
-            message, user["username"]
-        )
-        await ws.send(json.dumps(message_json))'''
-
-
-# Функція для обробки підключення до сервера та відправки ідентифікаційної інформації
-async def connect_to_server(ws):
-    global user
-    user = {
-        "username": create_username(),
-        "pub_key": serialize_pub_key(pub_key),
-    }
-    await ws.send(json.dumps(user))
-
-
-# Функція для обробки повідомлень від сервера
-async def handle_messages(ws):
-    async for message in ws:
-        data = json.loads(message)
-        print(data)
-        if "error" in data:
-            print(data["error"])
-        elif "state" in data:
-            global state
-            state = data["state"]
-            print(f"Перехід до стану {state}.")
-            if state == KEY_SETUP_PHASE1:
-                await handle_key_setup1(ws, data)
-            elif state == KEY_SETUP_PHASE2:
-                await handle_key_setup2(ws, data)
-            elif state == ACTIVE:
-                await handle_active_state(ws, data)
-
-
-# Функція для обробки стану KEY_SETUP_PHASE1
-async def handle_key_setup1(ws, data):
-    global participant_count
-    participant_count = data["number_of_participants"]
-    await ws.send(
-        json.dumps({"pub_key": serialize_pub_key(pub_key)})
-    )
-
-
-# Функція для обробки стану KEY_SETUP_PHASE2
-async def handle_key_setup2(ws, data):
-    global my_sym_key, my_salt, SHARED_KEY, encryption_ctrl
-    first_pub_key = deserialize_pub_key(
-        data["first_pub_key"]
-    )
-    shared_secret = dh.generate_shared_key(
-        priv_key, first_pub_key
-    )
-    my_sym_key, my_salt = dh.derive_key(shared_secret)
-    dec_key = dh.decrypt_message(
-        my_sym_key,
-        base64.b64decode(data["encrypted_K"].encode("utf-8")),
-    )
-
-    SHARED_KEY = dec_key
-    encryption_ctrl = EncryptionController(SHARED_KEY)
-    print(SHARED_KEY)
-    print("Encryption Controller:")
-    print(encryption_ctrl)
-
-    await ws.send(json.dumps({"state": ACTIVE}))
-
-
-# Функція для обробки стану ACTIVE
-async def handle_active_state(ws, data):
-    print("Перехід до активного стану.")
-    messages = [
-        "hi",
-        "hello",
-        "how are you?",
-        "i'm tired",
-    ]
-    for message in messages:
-        message_json = encryption_ctrl.create_message_json(
-            message, user["username"]
-        )
-        await ws.send(json.dumps(message_json))
-
-
-# Основна функція для підключення до сервера та обробки повідомлень
-async def main_send_messages():
     uri = "ws://localhost:8090"
-    async with websockets.connect(uri) as ws:
-        await connect_to_server(ws)
-        await handle_messages(ws)
+
+    # Підключення до WebSocket
+    async with websockets.connect(uri) as websocket:
+
+        # Генерація імені користувача та серіалізація публічного ключа
+        user = {
+            "username": generate_username(),
+            "public_key": serialize_key(public_key),
+        }
+
+        # Надсилання інформації користувача серверу
+        await websocket.send(json.dumps(user))
+
+        # Обробка повідомлень від сервера
+        async for message in websocket:
+            data = json.loads(message)
+            print(f"Отримано повідомлення - {data}.")
+
+            # Якщо в отриманих даних є помилка
+            if "error" in data:
+                print(f"Помилка! {data['error']}.")
+
+            # Обробка стану сервера
+            elif "state" in data:
+                global current_state
+                current_state = data["state"]
+                print(f"Перехід до стану {current_state}.")
+
+                # Обробка першої фази налаштування ключів
+                if current_state == KEY_SETUP_PHASE1:
+                    participants_number = data["participants_number"]
+                    await websocket.send(json.dumps({"public_key": serialize_key(public_key)}))
+
+                # Обробка другої фази налаштування ключів
+                elif current_state == KEY_SETUP_PHASE2:
+                    first_user_pub_key = deserialize_key(data["first_user_pub_key"])
+                    shared_key = dh.generate_shared_key(private_key, first_user_pub_key)
+                    my_sym_key, my_salt = dh.derive_key(shared_key)
+                    dec_key = dh.decrypt_message(
+                        my_sym_key,
+                        base64.b64decode(data["enc_key"].encode("utf-8")),
+                    )
+                    shared_key = dec_key
+                    encryptionController = EncryptionController(shared_key)
+                    print(f"Спільний ключ: {shared_key}.")
+                    print("Encryption Controller ініціалізовано.")
+                    print(encryptionController)
+
+                    await websocket.send(json.dumps({"state": ACTIVE}))
+
+                # Обробка активного стану
+                if current_state == ACTIVE:
+                    print("Перехід до активного стану.")
+                    messages = ["Hello, Bob", "Hi, Alice!", "Hey, Eva"]
+                    for message in messages:
+                        message_json = encryptionController.create_message_json(message, user["username"])
+                        await websocket.send(json.dumps(message_json))
+
+            # Обробка повідомлень для першого користувача
+            elif current_state == KEY_SETUP_PHASE1:
+                pub_keys[data["username"]] = deserialize_key(data["public_key"])
+                print("Публічні ключі учасників:")
+                print(pub_keys[data["username"]])
+                print(public_key)
+                print(private_key)
+                shared_keys[data["username"]] = dh.generate_shared_key(private_key, pub_keys[data["username"]])
+                sym_keys[data["username"]], salts[data["username"]] = dh.derive_key(shared_keys[data["username"]])
+                processed_participants_number += 1
+
+                # Якщо всі учасники оброблені, генерувати спільний ключ та перейти до другої фази
+                if processed_participants_number >= participants_number - 1:
+                    K = os.urandom(32)
+                    shared_key = K
+                    encryptionController = EncryptionController(shared_key)
+                    print(f"Спільний ключ: {shared_key}")
+                    await websocket.send(
+                        json.dumps(
+                            {
+                                "state": KEY_SETUP_PHASE2,
+                                "first_user_pub_key": serialize_key(public_key),
+                                "enc_key": {
+                                    k: base64.b64encode(dh.encrypt_message(v, K)).decode("utf-8")
+                                    for k, v in sym_keys.items()
+                                },
+                            }
+                        )
+                    )
+
+            # Обробка активного стану
+            elif current_state == ACTIVE:
+                json_data = json.loads(data)
+                encrypted_message = json_data["message"]
+                iv = json_data["init_vector"]
+                print("Зашифроване повідомлення:")
+                print(encrypted_message)
+                decrypted_message = encryptionController.decrypt(iv, encrypted_message)
+                print("Розшифроване повідомлення:")
+                print(decrypted_message)
+
 
 if __name__ == "__main__":
-    asyncio.run(main_send_messages())
+    asyncio.run(main())
--- server.py
+++ server.py
@@ -2,167 +2,133 @@ import json
 import asyncio
 import websockets
 
+# Константи для різних станів сервера
+WAITING = "WAITING"
+ACTIVE = "ACTIVE"
+KEY_SETUP_PHASE1 = "KEY_SETUP_PHASE1"
+KEY_SETUP_PHASE2 = "KEY_SETUP_PHASE2"
+current_state = WAITING  # Початковий стан сервера - очікування
 
-WAITING = "waiting"             # Початковий стан очікування
-ACTIVE = "active"               # Стан активності
-KEY_SETUP_PHASE1 = "setup1"     # Стан встановлення спільних ключів шифрування
-KEY_SETUP_PHASE2 = "setup2"     # Стан завершення підготовки
-state = WAITING
-
-connected_users_list = {}
-num_ready_users = 0             # Лічильник готових до переходу на наступний етап користувачів
-
-
-async def initial_connection(ws, user):
-    connected_users_list[ws] = user
-    print(f"Підключено користувача {user}.")
+connected_users_list = {}  # збереження підключених користувачів
+num_ready_users = 0  # Лічильник користувачів, готових до переходу в активний стан
 
 
-async def handle_key_setup1(ws, data):
-    global state
-    if "state" in data and data["state"] == KEY_SETUP_PHASE2:
-        state = KEY_SETUP_PHASE2
-        enc_key = data["enc_key"]
-        await send_encrypted_keys_to_users(ws, enc_key, data)
-    elif "public_key" in data:
-        await store_public_key_and_notify_first_user(ws, data)
-
-
-async def send_encrypted_keys_to_users(ws, enc_key, data):
-    for user in connected_users_list:
-        if user != ws:
-            await user.send(
-                json.dumps(
-                    {
-                        "state": state,
-                        "encrypted_key": enc_key[connected_users_list[user]["username"]],
-                        "first_user_public_key": data["first_user_public_key"],
-                    }
-                )
-            )
-
-
-async def store_public_key_and_notify_first_user(ws, data):
-    connected_users_list[ws]["public_key"] = data["public_key"]
-    first_user = list(connected_users_list.keys())[0]
-    if ws != first_user:
-        await first_user.send(
-            json.dumps(
-                {
-                    "public_key": data["public_key"],
-                    "username": connected_users_list[ws]["username"],
-                }
-            )
+# Функція для запуску додатку (перехід до фази налаштування ключів)
+async def run_app():
+    global current_state
+    current_state = KEY_SETUP_PHASE1  # Змінюємо стан на фазу налаштування ключів
+    await broadcast_except_sender(
+        json.dumps(
+            {"state": current_state, "participants_number": len(connected_users_list)}
         )
-
-
-async def handle_key_setup2(ws, data):
-    global state, num_ready_users
-    if "state" in data and data["state"] == ACTIVE:
-        num_ready_users += 1
-    if num_ready_users >= len(connected_users_list) - 1:
-        state = ACTIVE
-        await notify_users_of_active_state()
-
-
-async def notify_users_of_active_state():
-    for user in connected_users_list:
-        await user.send(json.dumps({"state": state}))
-
-
-async def handle_active_state(ws, data):
-    print("Перехід до активного стану.")
-    print(data)
-    await broadcast_except_sender(json.dumps(data), ws)
-
-
-async def handle_message(ws, msg):
-    data = json.loads(msg)
-    if state == KEY_SETUP_PHASE1:
-        await handle_key_setup1(ws, data)
-    elif state == KEY_SETUP_PHASE2:
-        await handle_key_setup2(ws, data)
-    elif state == ACTIVE:
-        await handle_active_state(ws, data)
-    else:
-        await ws.send(json.dumps({"error": "Чат ще не розпочато."}))
-
-
-async def handler(ws: websockets.WebSocketServerProtocol, path: str):
-    global state
-    try:
-        if state != WAITING:
-            await ws.send(
-                json.dumps(
-                    {
-                        "error": f"Нові підключення не дозволені. Поточний стан: {state}."
-                    }
-                )
-            )
-            await ws.close()
-            return
-
-        user = await ws.recv()
-        user = json.loads(user)
-        await initial_connection(ws, user)
-
-        async for msg in ws:
-            await handle_message(ws, msg)
-    except websockets.ConnectionClosed as e:
-        await disconnection(ws, e)
-    finally:
-        if ws in connected_users_list:
-            print(f"З'єднання з користувачем {connected_users_list[ws]} закрито.")
-            del connected_users_list[ws]
-
-
-async def disconnection(ws, e):
-    print(
-        f"Клієнт відключився: {ws.remote_address} з кодом {e.code} та причиною: {e.reason}."
     )
 
 
-# Функція для трансляції повідомлення всім користувачам, окрім відправника
+# Функція для обробки вводу команд з терміналу
+async def process_input(stop_event):
+    while True:
+        command = await asyncio.to_thread(input, "")  # Чекаємо вводу з терміналу
+        if command.lower() == "run" and current_state == "WAITING":
+            await run_app()
+            print("Перехід до стану підготовки.")
+        elif command.lower() == "quit":
+            print("Зупинка сервера.")
+            stop_event.set()  # Встановлюємо подію для зупинки сервера
+            break
+        else:
+            print(f"Введено невідому команду {command}. Доступні команди: run, quit.")
+
+
+# Функція для обробки повідомлень від користувачів в чаті
+async def process_chat(websocket: websockets.WebSocketServerProtocol, path: str):
+    global current_state, num_ready_users
+    try:
+        # Перевірка стану сервера, якщо не очікує - закриваємо з'єднання
+        if current_state != WAITING:
+            # Відправляємо повідомлення про заборону нових підключень
+            await websocket.send(
+                json.dumps(
+                    {"error": f"Нові підключення не дозволені. Поточний стан: {current_state}."}
+                )
+            )
+            await websocket.close()  # Закриваємо з'єднання
+            return
+
+        # Отримуємо дані користувача
+        user = await websocket.recv()  # Читаємо повідомлення від клієнта
+        user = json.loads(user)  # Парсимо JSON-повідомлення в словник
+        connected_users_list[websocket] = user  # Додаємо користувача до списку підключених
+        print(f"Підключено користувача {user}.")
+
+        # Обробляємо повідомлення від користувача
+        async for message in websocket:
+            data = json.loads(message)  # Парсимо отримане повідомлення
+            print(f"Отримане повідомлення - {data}.")
+
+            # Якщо сервер в фазі налаштування ключів (перша фаза)
+            if current_state == KEY_SETUP_PHASE1:
+                if "state" in data and data["state"] == KEY_SETUP_PHASE2:
+                    current_state = KEY_SETUP_PHASE2  # Перехід до другої фази налаштування ключів
+                    enc_key = data["enc_key"]  # Отриманий зашифрований ключ
+
+                    # Відправляємо зашифрований ключ іншим користувачам
+                    for user in connected_users_list:
+                        if user != websocket:
+                            await user.send(
+                                json.dumps(
+                                    {
+                                        "state": current_state,
+                                        "enc_key": enc_key[connected_users_list[user]["username"]],
+                                        "first_user_pub_key": data["first_user_pub_key"],
+                                    }
+                                )
+                            )
+                elif "public_key" in data:
+                    connected_users_list[websocket]["public_key"] = data["public_key"]  # Зберігаємо публічний ключ користувача
+                    first_user = list(connected_users_list.keys())[0]  # Отримуємо першого підключеного користувача
+                    if websocket != first_user:
+                        await first_user.send(
+                            json.dumps(
+                                {
+                                    "public_key": data["public_key"],
+                                    "username": user["username"],
+                                }
+                            )
+                        )
+            # Якщо сервер в фазі налаштування ключів (друга фаза)
+            elif current_state == KEY_SETUP_PHASE2:
+                if "state" in data and data["state"] == ACTIVE:
+                    num_ready_users += 1  # Збільшуємо лічильник готових користувачів
+                if num_ready_users >= len(connected_users_list) - 1:
+                    current_state = ACTIVE  # Перехід до активного стану
+                    for user in connected_users_list:
+                        await user.send(json.dumps({"state": current_state}))  # Відправляємо повідомлення про активний стан
+            # Якщо сервер в активному стані
+            elif current_state == ACTIVE:
+                await broadcast_except_sender(json.dumps(data), websocket)  # Трансляція повідомлення всім користувачам
+            else:
+                await websocket.send(json.dumps({"error": "Чат ще не розпочато."}))  # Повідомлення про те, що чат не розпочато
+    except websockets.ConnectionClosed as e:
+        print(f"Клієнт відключився {websocket.remote_address} з кодом {e.code} через {e.reason}.")
+    finally:
+        if websocket in connected_users_list:
+            print(f"З'єднання з користувачем {connected_users_list[websocket]} закрито.")
+            del connected_users_list[websocket]  # Видаляємо користувача зі списку підключених
+
+
+# Функція для трансляції повідомлень всім користувачам, крім відправника
 async def broadcast_except_sender(msg, sender=None):
-    if connected_users_list:  # Перевірка, чи є підключені користувачі
-        destinations = [
-            user.send(msg) for user in connected_users_list if user != sender
-        ]
+    if connected_users_list:
+        destinations = [user.send(msg) for user in connected_users_list if user != sender]
         if destinations:
             await asyncio.wait(destinations)
 
 
-# Функція для обробки команди "run" та переходу до стану "setup1"
-async def run_command():
-    global state
-    state = KEY_SETUP_PHASE1
-    await broadcast_except_sender(
-        json.dumps(
-            {"state": state, "number_of_participants": len(connected_users_list)}
-        )
-    )
-
-
-# Функція для обробки вводу з терміналу
-async def input_from_terminal(stop_event):
-    while True:
-        command = await asyncio.to_thread(input, "")
-        if command.lower() == "run" and state == "waiting":
-            await run_command()
-            print("Перехід до стану підготовки.")
-        elif command.lower() == "quit":
-            print("Зупинка сервера.")
-            stop_event.set()
-            break
-        else:
-            print(f"Невідома команда: {command}.")
-
-
-# Основна функція для запуску сервера та обробки вводу
+# Основна функція для запуску сервера
 async def main():
     stop_event = asyncio.Event()
-    server = await websockets.serve(handler, "localhost", 8090)
-    input_task = asyncio.create_task(input_from_terminal(stop_event))
+    server = await websockets.serve(process_chat, "localhost", 8090)
+    input_task = asyncio.create_task(process_input(stop_event))
 
     await stop_event.wait()
     server.close()
